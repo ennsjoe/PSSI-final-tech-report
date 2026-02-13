d2c15b24678768c817862a75e4e617c3b1e95ca2
ennsjoe <enns.joe@gmail.com>
2026-02-13 13:45:01 -0800

    Revised project id format

--- helper-functions/helper-functions.R
+++ helper-functions/helper-functions.R
@@ -478,8 +478,7 @@ make_project_banner <- function(
   ft <- flextable::set_table_properties(ft, layout = "fixed")
   
   ft
-} # Added missing closing brace
-
+}
 # --- ID Formatting Helper ----------------------------------------------------
 
 #' display_id
